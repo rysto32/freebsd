cb7106dc976956518e5d7e1324862b9dcd627e34
Ryan Stone <rstone@sandvine.com>
2014-04-15 23:09:06 -0400

    Allow passthrough devices to be hinted.
    Allow the ppt driver to attach to devices that were hinted to be
    passthrough devices by the PCI code creating them with a driver
    name of "ppt".
    
    Add a tunable that allows the IOMMU to be forced to be used.  With
    SR-IOV passthrough devices the VFs may be created after vmm.ko is
    loaded.  The current code will not initialize the IOMMU in that
    case, meaning that the passthrough devices can't actually be used.

--- sys/amd64/vmm/vmm.c
+++ sys/amd64/vmm/vmm.c
@@ -207,6 +207,11 @@ static int vmm_ipinum;
 SYSCTL_INT(_hw_vmm, OID_AUTO, ipinum, CTLFLAG_RD, &vmm_ipinum, 0,
     "IPI vector used for vcpu notifications");
 
+static int vmm_force_iommu = 0;
+TUNABLE_INT("hw.vmm.force_iommu", &vmm_force_iommu);
+SYSCTL_INT(_hw_vmm, OID_AUTO, force_iommu, CTLFLAG_RDTUN, &vmm_force_iommu, 0,
+    "Force use of I/O MMU even if no passthrough devices were found.");
+
 static void
 vcpu_cleanup(struct vm *vm, int i, bool destroy)
 {
@@ -304,7 +309,7 @@ vmm_handler(module_t mod, int what, void *arg)
 	switch (what) {
 	case MOD_LOAD:
 		vmmdev_init();
-		if (ppt_avail_devices() > 0)
+		if (vmm_force_iommu || ppt_avail_devices() > 0)
 			iommu_init();
 		error = vmm_init();
 		if (error == 0)
