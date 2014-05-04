26c0b31d1d2d0e018118654cd4015b1a99f4a5af
Ryan Stone <rstone@FreeBSD.org>
2014-05-03 21:17:40 -0400

    Add infrastructure for exporting config schema from PF drivers

--- sys/dev/pci/pci_private.h
+++ sys/dev/pci/pci_private.h
@@ -150,7 +150,8 @@ struct resource *pci_alloc_multi_resource(device_t dev, device_t child,
 		    int type, int *rid, u_long start, u_long end, u_long count,
 		    u_long num, u_int flags);
 
-int		pci_iov_attach_method(device_t bus, device_t dev);
+int		pci_iov_attach_method(device_t bus, device_t dev,
+		    struct nvlist *pf_schema, struct nvlist *vf_schema);
 int		pci_iov_detach_method(device_t bus, device_t dev);
 
 struct resource *pci_vf_alloc_mem_resource(device_t dev, device_t child,
