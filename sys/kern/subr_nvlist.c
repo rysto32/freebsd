c18e33591fd28378de0281041d41ea2750903dea
Ryan Stone <rstone@FreeBSD.org>
2014-12-29 13:57:33 -0500

    fixup! Fix serialization of empty sub-nvlists

--- sys/kern/subr_nvlist.c
+++ sys/kern/subr_nvlist.c
@@ -533,7 +533,6 @@ nvlist_xpack(const nvlist_t *nvl, int64_t *fdidxp, size_t *sizep)
 			ptr = nvpair_pack_string(nvp, ptr, &left);
 			break;
 		case NV_TYPE_NVLIST:
-			printf("Found nested nvlist %s -> %p\n", nvp->nvp_name, nvl);
 			nvl = nvpair_get_nvlist(nvp);
 			nvp = nvlist_first_nvpair(nvl);
 			ptr = nvlist_pack_header(nvl, ptr, &left);
@@ -558,12 +557,10 @@ nvlist_xpack(const nvlist_t *nvl, int64_t *fdidxp, size_t *sizep)
 		while ((nvp = nvlist_next_nvpair(nvl, nvp)) == NULL) {
 continue_parent:
 			nvp = nvlist_get_nvpair_parent(nvl);
-			printf("Done nvlist %p; try parent %p\n", nvl, nvp);
 			if (nvp == NULL)
 				goto out;
 			ptr = nvpair_pack_nvlist_up(ptr, &left);
 			if (ptr == NULL) {
-				printf("nvpair_pack_nvlist_up failed\n");
 				goto out;
 			}
 			nvl = nvlist_get_parent(nvl);
